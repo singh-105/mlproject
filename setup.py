e5ac4901a756aa1840792c335d7d09ef400e57a0
singh-105 <manojrsingh1974@gmail.com>
2026-01-02 13:10:42 +0530

    eda and prb statement

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->list[str]:
+
+HYPEN_E_DOT = '-e .'
+
+# FIX 1: Use List[str] instead of list[str]
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function will return the requriements 
+    this function will return the requirements 
     '''
-    requirements=[]
-    with open(file_path)as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -16,12 +19,10 @@ def get_requirements(file_path:str)->list[str]:
     return requirements
 
 setup(
-name='mlproject',
-version='0.0.1',
-author='harsh',
-author_email='manojrsingh1974@gmail.com',
-package=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
-
+    name='mlproject',
+    version='0.0.1',
+    author='harsh',
+    author_email='manojrsingh1974@gmail.com',
+    packages=find_packages(), # FIX 2: Changed 'package' to 'packages' (plural)
+    install_requires=get_requirements('requirements.txt')
 )
